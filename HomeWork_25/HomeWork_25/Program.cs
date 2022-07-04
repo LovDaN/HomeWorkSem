19e78a9b2fb7c07df93bde4e72e3315f73bef243
Daniil <lovaginDV@mail.ru>
2022-07-04 03:11:14 +0300

    Добавлен файл с номером 25

---
+++ HomeWork_25/HomeWork_25/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число (степень)");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Возведем число "+firstNum+" в степень "+secondNum);
+            int result = 1;
+            
+            for (int i = 0; i < secondNum; i++)
+            {
+                result = result * firstNum;
+                
+                
+            }
+            Console.WriteLine("Результат вычисления " + result);
+        }
+
+            
+        
+    }
+}
