b2b6247a67cc0ae5e2f5ad651d2f32d8fa9e3f29
Daniil <lovaginDV@mail.ru>
2022-07-06 03:58:59 +0300

    Добавлены файлы с номерами 27 и 29

---
+++ HomeWork_27/HomeWork_27/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            int quantity = ResNumbers(number);
+            Console.WriteLine("Количество цифр в числе ("+quantity+")");
+
+            int result = SumNumbers(number);
+            Console.WriteLine("Сумма цифр числа = " + result);
+        }
+        static int ResNumbers(int num)
+        {
+            int result = 0;
+            for (int i = 0; num!=0; i++)
+            {
+                result++;
+                num = num / 10;
+               
+            }
+            return result;
+        }
+        static int SumNumbers(int num)
+        {
+            int res = num;
+            int result=0;
+            int lastNum;
+            while (num != 0)
+            {
+                lastNum = res
+                    % 10;
+                result = result + lastNum;
+                num = num / 10;
+
+            }
+            return result;
+        }
+    }
+}
---
+++ HomeWork_29/HomeWork_29/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[8];
+            int[] result = RandArr(myArray);
+            PrintArr(result);
+        }
+        static int[] RandArr(int[] arr)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(0, 2);
+            }
+            return arr;
+        }
+        static void PrintArr(int[] arr)
+        {
+            for (int i = 0;i < arr.Length;i++)
+            {
+                Console.Write(arr[i]);
+                Console.WriteLine();
+            }
+            
+        }
+        
+    }
+}
