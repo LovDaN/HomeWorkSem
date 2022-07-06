515aa3b01dd433dff50967c723031059be822e34
Daniil <lovaginDV@mail.ru>
2022-07-06 03:49:16 +0300

    Отредактирован файл с номером 25

--- HomeWork_25/HomeWork_25/Program.cs
+++ HomeWork_25/HomeWork_25/Program.cs
@@ -10,23 +10,26 @@ namespace HomeWork_25
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите первое число");
+            Console.WriteLine("Введите первое число A");
             int firstNum = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите второе число (степень)");
+            Console.WriteLine("Введите второе число B (степень)");
             int secondNum = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Возведем число "+firstNum+" в степень "+secondNum);
-            int result = 1;
-            
-            for (int i = 0; i < secondNum; i++)
-            {
-                result = result * firstNum;
-                
-                
-            }
+            int result = Exponentiation(firstNum, secondNum);
             Console.WriteLine("Результат вычисления " + result);
+
+        }
+        static int Exponentiation(int num, int num2)
+        {
+            int result = 1;
+            for (int i = 0; i < num2; i++)
+            {
+                result = result * num;
+            }
+            return result;
         }
 
-            
-        
+
+
     }
 }
