efea9801a1d038a6870366d11dd66502cdb0aa38
Daniil <lovaginDV@mail.ru>
2022-07-16 08:40:24 +0300

    Добавлены файлы с номерами 41 и 43

---
+++ HomeWork_41/HomeWork_41/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace HomeWork_41
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во чисел М");
+            int count = Convert.ToInt32(Console.ReadLine());
+            int[] emptyArr = new int[count];
+            int[] fullArr = InputArray(emptyArr);
+            PrintArr(fullArr);
+            Console.WriteLine();
+            FindPositiveNums(fullArr);
+            
+
+        }
+        static int[] InputArray(int[] arr)
+        {
+
+            for(int i = 0; i<arr.Length;i++)
+            {
+                Console.WriteLine("введите число " +(i+1));
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                
+            }
+            Console.Clear();
+            return arr;
+            
+        }
+        static void PrintArr(int[] arr)
+        {
+            for(int i = 0; i<arr.Length;i++)
+            {
+                if (i == 0) { Console.Write("[");}
+                Console.Write(arr[i]);
+                if (i < arr.Length-1) { Console.Write(", ");}
+                if (i == arr.Length-1) { Console.Write("]");}
+            }
+        }
+        static void FindPositiveNums(int[] arr)
+        {
+            int count=0;
+            for( int i = 0; i < arr.Length;i++)
+            {
+                if(arr[i] > 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Положительных чисел до M = "+count);
+        }
+    }
+}
---
+++ HomeWork_43/HomeWork_43/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_43
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать!");
+            double[] pointsArr = InputValues();
+            double[] resultArr = FindPoints(pointsArr);
+            PrintResult(resultArr);
+       
+        }
+        static double[] InputValues()
+        {
+            Console.WriteLine("Введите по порядку точки b1, k1, b2 и k2");
+            double[] arr = new double[4];
+            for(int i = 0; i<arr.Length;i++)
+            {
+                Console.WriteLine("Введите точку " + (i + 1));
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Clear();
+            return arr;
+        }
+        static void PrintResult(double[] arr)
+        {
+            if (((arr[0]) - (arr[1])) == 0)
+            {
+                Console.WriteLine("Точка пересечения на координатах (" + arr[0] + ", " + arr[1] + ")");
+            }
+            else
+            {
+                Console.WriteLine("Точки пересечения не существует.");
+            }
+        }
+        static double[] FindPoints(double[] arr)
+        {
+            double[] pointsArr = new double[2];
+            pointsArr[0] =(arr[0] - arr[2]) / ((arr[1] - arr[3])*-1);
+            pointsArr[1] = arr[3] * (pointsArr[0]) + (arr[2]);
+            return pointsArr;
+        }
+
+    }
+}
