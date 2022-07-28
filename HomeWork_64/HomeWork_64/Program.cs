3c5093cf1ed65d72bdee75acccce4c6e8c885277
Daniil <lovaginDV@mail.ru>
2022-07-28 09:43:50 +0300

    Добавлены файлы с номерами 64, 66

---
+++ HomeWork_64/HomeWork_64/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_64
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Задайте число N: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            FindNaturalNumber(number);
+        }
+        static void FindNaturalNumber(int num)
+        {
+            if (num == 0) return;
+            Console.Write($"{num}  ");
+            FindNaturalNumber(num-1);
+        }
+    }
+}
---
+++ HomeWork_66/HomeWork_66/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_66
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Задайте число N: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Задайте число M: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            FindSumNaturalNumber(firstNumber, secondNumber);
+        }
+        static void FindSumNaturalNumber(int firstNum, int secondNum)
+        {
+            int result = 0;
+            
+            if (firstNum < secondNum)
+            {
+                while (firstNum < secondNum)
+                {
+                    if (result == 0) firstNum++;
+                    Console.Write($"{firstNum} ");
+                    result += firstNum;
+                    firstNum++;
+                }
+            }
+            else
+            {
+                while (secondNum < firstNum)
+                {
+                    if (result == 0) secondNum++;
+                    Console.Write($"{secondNum} ");
+                    result += secondNum;
+                    secondNum++;
+                }
+            }
+            Console.Write(" = "+result);
+            Console.WriteLine();
+            
+           
+        }
+    }
+}
